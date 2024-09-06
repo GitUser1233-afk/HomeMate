d16d1b3127e08e5571d551b507b4c3a4c6499c38
Anshika-singhal <anshikasinghal3014@gmail.com>
2024-09-06 20:28:28 +0530

    Changes in Code

--- routes/categories.js
+++ routes/categories.js
@@ -6,20 +6,29 @@ const Category =require('../models/category');
 
 //categorry api building
 
-router.post('/',async(req,res)=>{
-    const {name}=req.body;
-    try{
-        const category=new Category({name});
-        await category.save();
-        res.status(201).json({message:"Category created!!!!!!!!!"});
- 
-    }catch(err){
-        if(err.code===11000){
-            return res.status(400).json({message:"Category already exists"})
-        }
-        res.status(500).json({message:"server error", error:err.message})
+router.post('/', async (req, res) => {
+    let { name } = req.body;
+    name=name.toLowerCase();
+    name=name.trim();
+    name = name.replace(/\s+/g, ' ');
+
+    const isValidName = /^[a-zA-Z0-9 ]+$/.test(name);
+
+    if (!isValidName) {
+        return res.status(400).json({ message: 'Invalid category name! Only alphanumeric characters are allowed, without spaces or special symbols.' });
     }
-})
+
+    try {
+        const category = new Category({ name });
+        await category.save();
+        res.status(201).json({ message: "Category created successfully!" });
+    } catch (err) {
+        if (err.code === 11000) {
+            return res.status(400).json({ message: "Category already exists" });
+        }
+        res.status(500).json({ message: "Server error", error: err.message });
+    }
+});
 router.get('/getall',async(req,res)=>{
     try{
         const categories=await Category.find();
