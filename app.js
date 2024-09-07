682a2111bc661e04bdcdb59f3189c79e07b1a194
Anshika-singhal <anshikasinghal3014@gmail.com>
2024-09-07 16:51:10 +0530

    Changes in file

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-
 const express=require('express');
 const mongoose=require('mongoose');
 const bodyParser =require('body-parser');
 const categories=require('./routes/categories');
 const app=express();
+const mongourl=process.env.database;
 //work as a middleware
 app.use(bodyParser.json());
 //mongoose.connect
-mongoose.connect('mongodb+srv://Anshika_3012:Anshika%403012@cluster0.rrcc1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    {
-        //useNewurlParser:true,
-      
-    }
-)
+mongoose.connect(mongourl)
 .then(()=>console.log('mongoDb contted'))
 .catch(err=>console.log(err));
 
